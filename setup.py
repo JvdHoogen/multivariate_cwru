0b81e811db78c4cabd86fa089c8a1ba0a9786e72
JvdHoogen <57524082+JvdHoogen@users.noreply.github.com>
2020-12-02 15:07:10 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="multivariate_cwru",
+    version="0.0.2",
+    author="Jurgen van den Hoogen",
+    author_email="jurgenvandenhoogen@hotmail.com",
+    description="Preprocessed CWRU Bearing Dataset for multivariate signals",
+    url="https://github.com/JvdHoogen/multivariate_cwru",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",  
+    ],
+    python_requires='>=3.6',
+    include_package_data=True
+)
+
+
+
